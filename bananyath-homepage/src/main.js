66257fb7706318c06db2ce682a2152be9934a04e
Leonardo Madona <leonardo.madona@rd3.digital>
2026-06-12 00:02:13 -0300

    added bananyath model

--- bananyath-homepage/src/main.js
+++ bananyath-homepage/src/main.js
@@ -1,60 +1,68 @@
-import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OutlineEffect } from 'three/addons/effects/OutlineEffect.js';
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+let bananyathModel;
 
-<div class="ticks"></div>
+const scene = new THREE.Scene();
+const mainCanvas = document.getElementById('3dcanvas')
+const camera = new THREE.PerspectiveCamera(
+  60,
+  mainCanvas.width / mainCanvas.height,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer({canvas: mainCanvas, alpha: true});
+renderer.setSize( mainCanvas.width, mainCanvas.height );
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+camera.position.z = 8;
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+const loader = new GLTFLoader();
 
-setupCounter(document.querySelector('#counter'))
+loader.load( 'src/assets/halloweenNyath.glb', function ( gltf ) {
+
+  bananyathModel = gltf.scene;
+
+
+  /*bananyathModel.traverse((o) => {
+    if(o.isMesh)
+    {
+      let toonMaterial = new THREE.MeshToonMaterial({
+        color : o.material.color, 
+        map: o.material.map
+      });
+      o.material = toonMaterial;
+    }
+  })*/
+
+  scene.add(bananyathModel);
+
+}, undefined, function (error) {
+
+  console.error(error);
+
+} );
+
+const dirLight = new THREE.DirectionalLight(0xffffff, 2);
+dirLight.position.set(1,1,1);
+dirLight.target.position.set(0,0,0);
+
+scene.add(dirLight);
+scene.add(dirLight.target);
+
+const effect = new OutlineEffect( renderer, {defaultThickness: 0.005});
+
+function animate(time)
+{
+  if(bananyathModel)
+  {
+    bananyathModel.rotation.y = time / 2000;
+    bananyathModel.rotation.x = .5 * Math.sin(time / 5000);
+  }
+
+  //renderer.render(scene, camera);
+  effect.render( scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
+
