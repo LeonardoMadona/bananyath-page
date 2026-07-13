fea84df916033b666da5bdbda9f0529db9695d78
Leonardo Madona <leonardo.madona@rd3.digital>
2026-07-13 07:51:27 -0300

    implemented carrousel. To-do: add the carrousel dots.

--- src/script.js
+++ src/script.js
@@ -1,18 +1,36 @@
 
 window.addEventListener("load", ()=>{
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
 
-    document.getElementById("customize-button").addEventListener('click', () => {
-        Customize();
-    })
+    if(page == '')
+    {
+        document.getElementById("customize-button").addEventListener('click', () => {
+            Customize();
+        })
+        
+        InstantiateCarrousel();
 
-    InstantiateCarrousel();
+        document.getElementById("carrousel-prev").addEventListener('click', () => {
+            MoveCarrousel(-1);
+        })
+
+        document.getElementById("carrousel-next").addEventListener('click', () => {
+            MoveCarrousel(1);
+        })
+    }
+    else if(page =='customize.html')
+    {
+        //do customize page stuff
+    }
+
 })
 
 let imagePaths = [
     'images/b1.jpg',
     'images/b2.jpg',
     'images/b3.jpg',
-    'images/b4.jpg'
+    'images/b4.png'
 ]
 
 let carrouselIndex = 0;
@@ -24,7 +42,7 @@ function Customize()
 
 function InstantiateCarrousel()
 {
-    var carrouselContainer = document.getElementById("carrouselDiv");
+    var carrouselContainer = document.getElementById("images-holder");
     carrouselIndex = 0;
 
 
@@ -41,7 +59,23 @@ function InstantiateCarrousel()
     }
 }
 
-function MoveCarrousel()
+function MoveCarrousel( direction )
 {
     //when we click, move carrousel to the next position.
+
+    carrouselIndex = (carrouselIndex + direction) % imagePaths.length;
+
+    if(carrouselIndex < 0)
+    {
+        carrouselIndex = imagePaths.length + carrouselIndex;
+    }
+
+    console.log(carrouselIndex)
+
+    var imageHolder = document.getElementById('images-holder');
+
+    imageHolder.style.transform = `translateX(-${carrouselIndex * imageHolder.parentElement.clientWidth}px)`;
+
+    console.log('moving with offset ' + direction);
+
 }
