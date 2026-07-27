0b8fd84d39c0bfce034b2647a0dfdf12067801d0
Leonardo Madona <leonardo.madona@rd3.digital>
2026-07-26 21:21:45 -0300

    added customize.html to build through vite.config.js settings.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [],
   base: '/bananyath-page/', 
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        customize: resolve(__dirname, 'customize.html'),
+      },
+    },
+  }
 })
